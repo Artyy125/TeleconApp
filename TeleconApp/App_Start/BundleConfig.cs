b2c01e3d1b4b4cbfbd7927527e43c8b8aaa81ac6
Alireza Torkamani <na.s.taaherian@hotmail.com>
2017-12-31 17:23:17 -0500

    completing splicer prep and test pages

--- TeleconApp/App_Start/BundleConfig.cs
+++ TeleconApp/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace TeleconApp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js", "~/Scripts/jquery-ui.min"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,7 +25,8 @@ namespace TeleconApp
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/Site.css"));
+                      "~/Content/site.css", "~/Content/jquery-ui.min.css",
+                      "~/Content/jquery-ui.theme.min.css", "~/Content/jquery-ui.min"));
         }
     }
 }
